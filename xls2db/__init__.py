cc1a987ebb0106adb74b23071088353c85194b92
John Reiser <jreiser@njgeo.org>
2014-10-21 14:06:40 -0400

    modified to produce Oracle-flavored SQL
    Quick and dirty. Use at your own risk.
    Modified to produce SQL that can be used in Oracle. Added ability to
    determine basic types (double, timestamp, else defaults to VARCHAR2).

--- xls2db/__init__.py
+++ xls2db/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import sys, os, itertools, logging
-import sqlite3 as sqlite
-
+import sys, os, itertools, logging, datetime
 import xlrd
 
 
@@ -22,7 +20,7 @@ log.setLevel(logging.ERROR)
 
 def xls2db(infile, outfile=None, column_name_start_row=0, data_start_row=1):
     """
-    Convert an xls file into an sqlite db!
+    Convert an xls file into an Oracle db!
     """
     #Now you can pass in a workbook!
     if isinstance(infile, string_types):
@@ -33,58 +31,73 @@ def xls2db(infile, outfile=None, column_name_start_row=0, data_start_row=1):
     else:
         raise TypeError("infile must be a string or xlrd.Book")
 
-    #Now you can pass in a sqlite connection!
+    #Now you can pass in a Oracle connection!
     if outfile is None:
-        outfile = os.path.splitext(infile)[0] + '.sqlite'
+        raise NotImplementedError("outfile must be a string")
 
     if isinstance(outfile, string_types):
-        db_conn = sqlite.connect(outfile)
-        db_cursor = db_conn.cursor()
-    elif isinstance(outfile, sqlite.Connection):
-        db_conn = outfile
-        db_cursor = db_conn.cursor()
+        outfile = outfile
     else:
-        raise TypeError("outfile must be a string or sqlite.Connection")
+        raise TypeError("outfile must be a string or cx_Oracle.Connection")
+
+    of = open(outfile, 'w')
 
     # hack, avoid plac's annotations....
     column_name_start_row = int(column_name_start_row)
     data_start_row = int(data_start_row)
     for s in wb.sheets():
-
         # Create the table.
         # Vulnerable to sql injection because ? is only able to handle inserts
         # I'm not sure what to do about that!
         if s.nrows > column_name_start_row:
             column_names = []
+            column_types = []
             for j in range(s.ncols):
                 # FIXME TODO deal with embedded double quotes
                 colname = s.cell(column_name_start_row, j).value
                 if colname:
                     colname = '"c%d"' % (colname,) if isinstance(colname, float) else \
                               u'"%s"' % (colname,)
+                    colname = colname.replace(" ","_")
                 else:
                     colname = '"col%d"' % (j + 1,)
                 # FIXME TODO deal with embedded spaces in names
                 # (requires delimited identifiers) and missing column types
+                coltype = s.cell(data_start_row, j).value
+                if coltype:
+                    if isinstance(coltype, float):
+                        if colname[-5:] == 'Date"':
+                            coltype = "TIMESTAMP"
+                        elif colname[-5:] == 'Time"':
+                            coltype = "TIMESTAMP"
+                        else:
+                            coltype = 'DOUBLE PRECISION'
+                    else:
+                        coltype = 'VARCHAR2(1000)'
+                else:
+                    coltype = 'VARCHAR2(1000)'
                 column_names.append(colname)
-
-            column_names = ','.join(column_names)
-            tmp_sql = u'create table "' + s.name + '" (' + column_names + ');'
-            log.debug('DDL %r', tmp_sql)
-            db_cursor.execute(tmp_sql)
+                column_types.append(coltype)
+                #print zip(column_names, column_types)
+            column_names = ',\n'.join(map(lambda x: ' '.join(x), zip(column_names, column_types)))
+            tmp_sql = u'create table "' + s.name + '" (' + column_names + ');\n'
+            of.write(tmp_sql)
 
         if s.nrows > data_start_row:
-            tmp_sql = 'insert into "' + s.name + '" values (' +','.join(itertools.repeat('?', s.ncols)) +");"
+#            tmp_sql = 'insert into "' + s.name + '" values (' +','.join(itertools.repeat('?', s.ncols)) +");"
             for rownum in range(data_start_row, s.nrows):
-                bind_params = s.row_values(rownum)
-                log.debug('DML %r, %r', tmp_sql, bind_params)
-                db_cursor.execute(tmp_sql, bind_params)
-
-    db_conn.commit()
-    #Only do this if we're not working on an externally-opened db
-    if isinstance(outfile, string_types):
-        db_cursor.close()
-        db_conn.close()
+                of.write('insert into "' + s.name + '" values (')
+                valcol = []
+                for i, rv in enumerate(s.row_values(rownum)):
+                    if isinstance(rv, float):
+                        if(column_types[i] == 'TIMESTAMP'):
+                            valcol.append("TO_TIMESTAMP('"+datetime.datetime(*xlrd.xldate_as_tuple(rv, wb.datemode)).isoformat(' ')+"', 'YYYY-MM-DD HH24:MI:SS')")
+                        else:
+                            valcol.append(str(rv))
+                    else:
+                        valcol.append("'"+rv.replace("'","''")+"'") # I know.
+                of.write(", ".join(valcol))
+                of.write(");\n")
 
 
 def db2xls(infile, outfile):
